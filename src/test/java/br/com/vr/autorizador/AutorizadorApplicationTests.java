7d66589aa9cb3bde633b3f58770c3a4027d5b485
Diogo Japiassu <diogo.japiassu@sysmap.com.br>
2022-03-15 19:38:52 -0300

    Spring Initializr Template

---
+++ src/test/java/br/com/vr/autorizador/AutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vr.autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
